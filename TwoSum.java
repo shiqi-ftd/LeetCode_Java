0d46f5c69000d21558ab450555bc9dcc1a255e20
Shiqi Zhong <szhong4@vols.utk.edu>
2016-02-29 10:48:33 -0500

    TwoSum

--- TwoSum.java
+++ TwoSum.java
@@ -1,13 +1,16 @@
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
-		for (int i = 0; i < nums.length; i++) {
-			if (hm.containsKey(nums[i])) {
-				return new int[]{hm.get(nums[i]) + 1, i + 1 };
-			} else {
-				hm.put(target - nums[i], i);
-			}
-		}
-        return new int[2];
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+	//  contains *KEY*
+            if (hm.containsKey(nums[i])) {
+                return new int[]{hm.get(nums[i]), i};
+            } else{
+	// PUT not add for hashmap
+                hm.put(target - nums[i], i);
+            }
+        }
+        return new int[]{0};
     }
 }
