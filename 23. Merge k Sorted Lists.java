3cdba823415c3325205e7f36f4d83d2bfd0fd95c
Shiqi Zhong <szhong4@vols.utk.edu>
2016-03-13 15:52:50 -0400

    Update

--- 23. Merge k Sorted Lists.java
+++ 23. Merge k Sorted Lists.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -53,4 +54,61 @@ public class Solution {
         }
         return dummy.next;
     }
+=======
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+        return mergeHelper(lists, 0, lists.length - 1);
+    }
+    
+    private ListNode mergeHelper(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
+        }
+        
+        int mid = start + (end - start) / 2;
+        ListNode left = mergeHelper(lists, start, mid);
+        ListNode right = mergeHelper(lists, mid + 1, end);
+        return merge(left, right);
+    }
+
+    
+    private ListNode merge(ListNode node1, ListNode node2) {
+        if (node1 == null) {
+            return node2;
+        }
+        if (node2 == null) {
+            return node1;
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        while (node1 != null && node2 != null) {
+            if (node1.val <= node2.val) {
+                head.next = node1;
+                node1 = node1.next;
+            } else {
+                head.next = node2;
+                node2 = node2.next;
+            }
+            head = head.next;
+        }
+        if (node2 != null) {
+            head.next = node2;
+        }
+        if (node1 != null) {
+            head.next = node1;
+        }
+        return dummy.next;
+    }
+>>>>>>> Stashed changes
 }
